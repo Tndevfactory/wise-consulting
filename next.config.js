bbdbf7aea9d4acfae36c99153e52b99f1ba878b9
Tndevfactory <tndevfactory@gmail.com>
2021-12-28 21:27:23 +0100

    change header

--- next.config.js
+++ next.config.js
@@ -5,6 +5,23 @@ const nextConfig = {
   /* config options here */
   fallback: false,
   poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Powered-By',
+            value: 'Something',
+          },
+          {
+            key: 'Server',
+            value: 'Something',
+          },
+        ],
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
